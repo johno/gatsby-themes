874ffb77de1e1043b98443091c15054ac80b077b
John Otander <johnotander@gmail.com>
2019-01-02 14:01:10 -0700

    Configure now deployment

--- examples/blog/gatsby-config.js
+++ examples/blog/gatsby-config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  siteMetadata: {
+    title: 'Minimal blog theme example',
+    author: 'John Otander',
+    description: 'Example usage of gatsby-theme-minimal-blog',
+    siteUrl: 'https://gatsby-themes.johno.com/examples/blog'
+  },
+  pathPrefix: '/examples/blog',
   __experimentalThemes: [
     {
       resolve: 'gatsby-theme-minimal-blog',
